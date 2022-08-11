6b834250b2ef411d47f3b35f6dabe27c60ddfba0
Deya Card <thedeyacard@gmail.com>
2022-08-11 12:12:32 -0700

    add eventlist and write initial code

---
+++ src/components/EventList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Event from "./Event";
+import PropTypes from "prop-types";
+
+function EventList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.eventList.map((event) =>
+        <Event
+          whenEventClicked = {props.onEventSelection}
+          name={event.name}
+          location={event.location}
+          price={event.price}
+          gift={event.gift}
+          id={event.id}
+          key={event.key}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+EventList.propTypes = {
+  eventList: PropTypes.array,
+  onEventSelection: PropTypes.func
+};
+
+export default EventList;
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <React.Fragment>
       <h1 style={{backgroundColor: "darkblue", color: "white"}}>Ol Reign Ticket Sales</h1>
         <div>
-          <img src={eventImage} alt="An image of Tobin Heath in Lumen Stadium" class="center"/>
+          <img src={eventImage} alt="An image of Tobin Heath in Lumen Field" class="center"/>
         </div>
     </React.Fragment>
   );
